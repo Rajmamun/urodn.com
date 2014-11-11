feda8a7dafaba97395c04e4466d54962de481e4d
Md. Ainul Moin Noor <freak.arian@gmail.com>
2014-11-12 02:52:52 +0600

    About us
    Wanna upload

---
+++ about_us.php
@@ -0,0 +1,110 @@
+<!--header starts-->
+<?php
+require_once('header.php');
+?>
+<!--header ends-->
+
+
+ <!-- Fixed navbar starts-->
+ <?php
+ 	require_once('menu.php');
+ ?>
+ <!--Fixed navbar ends-->
+
+
+
+<!--Body Part Start-->
+<!--About Us Description Start-->
+<div class="container_other top_margin">
+	<div class="row">
+		<div class="panel panel-default ">
+		  <!-- Default panel contents -->
+		  	<div class="panel-heading">About Us</div>
+		  	<div class="panel-body">
+		    <p>Founded in 2013 and headquartered in Khulna, Bangladesh, URO Design Network is the fastest growing microstock agency in the world. We serve customers from over 192 countries, giving them support in 14 languages. Being a creative content marketplace, URO Design Network allows everyone to buy and sell high-quality stock photos, vector images, and videos.</p>
+		  	</div>
+		</div>
+	</div>
+</div>
+<!--About Us Description End-->
+
+<!-- Employee Part Start-->
+<div class="container_other top_margin">
+	<div class="row">
+		<div class="panel panel-default ">
+		  <!-- Default panel contents -->
+		  	<div class="panel-heading">Leadership</div>
+		  	<div class="panel-body">
+		    	<div class="row fond_size_employer">
+		    		<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp; CEO</p>
+					</div>
+<!---------------------------------Just Demo Start-------------------------------->
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp;  CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp;  CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp;  CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp;  CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp;  CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp;  CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp; CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp;  CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp; CEO</p>
+					</div>
+					<div class="col-md-2"><a href="#"><img alt="No Preview"  src="images/employee.jpg" width="100%"></a>
+						<p>Jon Oringer
+					    <br/>Founder &amp; CEO</p>
+					</div>
+<!---------------------------------Just Demo End-------------------------------->
+
+		    	</div>
+
+		  	</div>
+		</div>
+	</div>
+</div>
+<!-- Employee Part End-->
+
+<!-- Body Part End-->
+
+
+
+<hr>
+
+<!--Footer Start-->
+<?php
+require_once('footer.php');
+?>
+<!--Footer  End-->
+
+<!--Bottom Footer Start-->
+<?php
+require_once('bottom_footer.php');
+?>
+<!--Bottom Footer End-->
